351af063639eb793b87ab3a183788beffd9f09ab
Daniel Gasienica <daniel@gasienica.ch>
2018-03-08 00:21:48 -0500

    Parse optional `Attachment` fields

--- purescript/src/Signal/Types/Attachment.purs
+++ purescript/src/Signal/Types/Attachment.purs
@@ -18,37 +18,74 @@ import Data.Record.ShowRecord (showRecord)
 import Data.String (Pattern(..), replaceAll, Replacement(..))
 import Data.Traversable (traverse)
 
-newtype AttachmentData = AttachmentData ArrayBuffer
 
 newtype Attachment = Attachment
-  { contentType :: String
-  , data :: AttachmentData
-  , fileName :: String
-  , schemaVersion :: Maybe Int
+  -- Required
+  { fileName :: String
+  , contentType :: String
   , size :: Int
+  , data :: AttachmentData
+  -- Optional
+  , schemaVersion :: Maybe Int
+  , id :: Maybe String
+  , width :: Maybe Int
+  , height :: Maybe Int
+  , thumbnail :: Maybe AttachmentArrayBuffer
+  , key :: Maybe AttachmentArrayBuffer
+  , digest :: Maybe AttachmentArrayBuffer
+  , flags :: Maybe Int
   }
+
+newtype AttachmentData = AttachmentData ArrayBuffer
+newtype AttachmentArrayBuffer = AttachmentArrayBuffer ArrayBuffer
 
 instance showAttachment :: Show Attachment where
   show (Attachment o) = showRecord o
 
 instance showAttachmentData :: Show AttachmentData where
-  show (AttachmentData x) = "(AttachmentData { length: " <> show (byteLength x) <> " })"
+  show (AttachmentData x) =
+    "(AttachmentData { length: " <> show (byteLength x) <> " })"
+
+instance showAttachmentArrayBuffer :: Show AttachmentArrayBuffer where
+  show (AttachmentArrayBuffer x) =
+    "(AttachmentArrayBuffer { length: " <> show (byteLength x) <> " })"
 
 -- Parsing
 readAttachment :: Foreign -> F Attachment
 readAttachment value = do
-  contentType   <- value ! "contentType" >>= readString
-  data_         <- value ! "data" >>= unsafeReadTagged "ArrayBuffer"
-  fileName      <- value ! "fileName" >>= readString
-  size          <- value ! "size" >>= readInt
-  schemaVersion <- value ! "schemaVersion" >>= readNullOrUndefined >>= traverse readInt
-  pure $ Attachment
-    { contentType: contentType
-    , data: AttachmentData data_
-    , fileName: fileName
-    , size: size
-    , schemaVersion: schemaVersion
-    }
+    -- Required
+    fileName      <- value ! "fileName" >>= readString
+    contentType   <- value ! "contentType" >>= readString
+    size          <- value ! "size" >>= readInt
+    data_         <- value ! "data" >>= readArrayBuffer
+    -- Optional
+    schemaVersion <- value ! "schemaVersion" >>= readNullOrUndefined >>= traverse readInt
+    id_           <- value ! "id" >>= readNullOrUndefined >>= traverse readString
+    width         <- value ! "width" >>= readNullOrUndefined >>= traverse readInt
+    height        <- value ! "height" >>= readNullOrUndefined >>= traverse readInt
+    thumbnail     <- value ! "thumbnail" >>= readNullOrUndefined >>=
+                                traverse readArrayBuffer
+    key           <- value ! "key" >>= readNullOrUndefined >>= traverse readArrayBuffer
+    digest        <- value ! "digest" >>= readNullOrUndefined >>= traverse readArrayBuffer
+    flags         <- value ! "flags" >>= readNullOrUndefined >>= traverse readInt
+    pure $ Attachment
+      -- Required
+      { fileName: fileName
+      , contentType: contentType
+      , size: size
+      , data: AttachmentData data_
+      -- Optional
+      , schemaVersion: schemaVersion
+      , id: id_
+      , width: width
+      , height: height
+      , thumbnail: AttachmentArrayBuffer <$> thumbnail
+      , key: AttachmentArrayBuffer <$> key
+      , digest: AttachmentArrayBuffer <$> digest
+      , flags: flags
+      }
+  where
+    readArrayBuffer = unsafeReadTagged "ArrayBuffer"
 
 -- Schema
 leftToRightOverride :: Pattern
