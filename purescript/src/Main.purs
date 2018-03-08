6417fbaadd6647dbb2f6ceff468b5d0504742c2a
Daniel Gasienica <daniel@gasienica.ch>
2018-03-07 23:34:27 -0500

    Wire up message parsing to IndexedDB

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -1,13 +1,14 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
+
 import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Aff.Console (CONSOLE, log)
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Except (runExcept)
+import Data.Foreign (Foreign)
 import Data.Maybe (Maybe(..), maybe)
-import Data.Record.ShowRecord (showRecord)
-
 import Database.IndexedDB.Core            as IDB
 import Database.IndexedDB.IDBDatabase     as IDBDatabase
 import Database.IndexedDB.IDBFactory      as IDBFactory
@@ -15,41 +16,15 @@ import Database.IndexedDB.IDBKeyRange     as IDBKeyRange
 import Database.IndexedDB.IDBObjectStore  as IDBObjectStore
 import Database.IndexedDB.IDBTransaction  as IDBTransaction
 
+import Signal.Types.Message (readMessage)
+
 launchAff' :: forall a e. Aff e a -> Eff (exception :: EXCEPTION | e) Unit
 launchAff' aff =
   pure unit <* (launchAff aff)
 
-
-type Attachment =
-  { contentType :: String
-  -- , data :: ArrayBuffer
-  -- , digest :: ArrayBuffer
-  , fileName :: String
-  -- , flags :: Maybe Int
-  -- , height :: Maybe Int
-  -- , id :: String
-  -- , key :: ArrayBuffer
-  , schemaVersion :: Maybe Int
-  , size :: Int
-  -- , thumbnail :: Maybe {}
-  -- , width :: Maybe Int
-  }
-type Message =
-  -- { attachments :: Array Attachment
-  { body :: String
-  -- , conversationId :: String
-  -- , decrypted_at :: Int
-  -- , flags :: Int
-  -- , id :: String
-  -- , received_at :: Int
-  -- , recipients :: Array String
-  -- , sent :: Boolean
-  -- , sent_at :: Int
-  , source :: String
-  , sourceDevice :: Int
-  , timestamp :: Int
-  , type :: String -- 'outgoing'
-  }
+sampleMessageId :: String
+-- sampleMessageId = "069f009b-0c13-f4d7-0fbd-5d81db37d893" -- incoming
+sampleMessageId = "f9c4ef48-0fc9-7c21-3855-ab836fe15ef5" -- outgoing
 
 main :: Eff (idb :: IDB.IDB, exception :: EXCEPTION, console :: CONSOLE) Unit
 main = launchAff' do
@@ -61,6 +36,6 @@ main = launchAff' do
 
   tx    <- IDBDatabase.transaction db ["messages"] IDB.ReadOnly
   store <- IDBTransaction.objectStore tx "messages"
-  (maybeMessage :: Maybe Message) <-
-    IDBObjectStore.get store (IDBKeyRange.only "1052563c-a81d-1477-e69e-353a05850400")
-  log $ "[PureScript] message: " <> maybe "(message not found)" showRecord maybeMessage
+  (maybeMessage :: Maybe Foreign) <- IDBObjectStore.get store (IDBKeyRange.only sampleMessageId)
+  log $ "[PureScript] message: " <> maybe "(message not found)"
+    (show <<< runExcept <<< readMessage) maybeMessage
