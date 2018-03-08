fb0b0509b2f89a998552cc47051af22b038b14d0
Daniel Gasienica <daniel@gasienica.ch>
2018-03-08 00:01:01 -0500

    Implement `Attachment` parsing

--- purescript/src/Signal/Types/Attachment.purs
+++ purescript/src/Signal/Types/Attachment.purs
@@ -1,19 +1,56 @@
-module Signal.Types.Attachment where
+module Signal.Types.Attachment
+  ( Attachment
+  , readAttachment
+  , replaceUnicodeOrderOverrides
+  , replaceUnicodeOrderOverridesSync
+  )
+  where
 
 import Prelude
 
 import Control.Monad.Promise (PurePromise, resolve)
+import Data.ArrayBuffer.ArrayBuffer (byteLength)
 import Data.ArrayBuffer.Types (ArrayBuffer)
+import Data.Foreign (F, Foreign, readInt, readNullOrUndefined, readString, unsafeReadTagged)
+import Data.Foreign.Index ((!))
+import Data.Maybe (Maybe)
+import Data.Record.ShowRecord (showRecord)
 import Data.String (Pattern(..), replaceAll, Replacement(..))
+import Data.Traversable (traverse)
 
-type Attachment =
+newtype AttachmentData = AttachmentData ArrayBuffer
+
+newtype Attachment = Attachment
   { contentType :: String
-  , data :: ArrayBuffer
+  , data :: AttachmentData
   , fileName :: String
-  , size :: Number
-  , schemaVersion :: Number
+  , schemaVersion :: Maybe Int
+  , size :: Int
   }
 
+instance showAttachment :: Show Attachment where
+  show (Attachment o) = showRecord o
+
+instance showAttachmentData :: Show AttachmentData where
+  show (AttachmentData x) = "(AttachmentData { length: " <> show (byteLength x) <> " })"
+
+-- Parsing
+readAttachment :: Foreign -> F Attachment
+readAttachment value = do
+  contentType   <- value ! "contentType" >>= readString
+  data_         <- value ! "data" >>= unsafeReadTagged "ArrayBuffer"
+  fileName      <- value ! "fileName" >>= readString
+  size          <- value ! "size" >>= readInt
+  schemaVersion <- value ! "schemaVersion" >>= readNullOrUndefined >>= traverse readInt
+  pure $ Attachment
+    { contentType: contentType
+    , data: AttachmentData data_
+    , fileName: fileName
+    , size: size
+    , schemaVersion: schemaVersion
+    }
+
+-- Schema
 leftToRightOverride :: Pattern
 leftToRightOverride = Pattern "\x202D"
 
@@ -24,8 +61,8 @@ overrideReplacement :: Replacement
 overrideReplacement = Replacement "\xFFFD"
 
 replaceUnicodeOrderOverridesSync :: Attachment -> Attachment
-replaceUnicodeOrderOverridesSync attachment =
-    attachment { fileName = replaceOverrides attachment.fileName }
+replaceUnicodeOrderOverridesSync (Attachment attachment) =
+    Attachment $ attachment { fileName = replaceOverrides attachment.fileName }
   where
     replaceOverrides = replaceLTRO >>> replaceRTLO
     replaceLTRO = replaceAll leftToRightOverride overrideReplacement
