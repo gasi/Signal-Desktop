3e5df353824d2cf06ebb3427f53fea29d0e7b2d0
Daniel Gasienica <daniel@gasienica.ch>
2018-03-08 21:17:45 -0500

    Extract `databaseVersion`

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -22,6 +22,9 @@ launchAff' :: forall a e. Aff e a -> Eff (exception :: EXCEPTION | e) Unit
 launchAff' aff =
   pure unit <* (launchAff aff)
 
+databaseVersion :: Maybe Int
+databaseVersion = Just 16
+
 sampleMessageId :: String
 -- sampleMessageId = "069f009b-0c13-f4d7-0fbd-5d81db37d893" -- incoming
 sampleMessageId = "f9c4ef48-0fc9-7c21-3855-ab836fe15ef5" -- outgoing
@@ -31,8 +34,7 @@ main = launchAff' do
   let callbacks = { onBlocked : Nothing
                   , onUpgradeNeeded : Nothing
                   }
-      version = Just 16
-  db <- IDBFactory.open "signal" version callbacks
+  db <- IDBFactory.open "signal" databaseVersion callbacks
 
   tx    <- IDBDatabase.transaction db ["messages"] IDB.ReadOnly
   store <- IDBTransaction.objectStore tx "messages"
