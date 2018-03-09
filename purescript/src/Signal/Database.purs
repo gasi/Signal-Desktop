419fd41080885be69919a64ded2214ffd1231975
Daniel Gasienica <daniel@gasienica.ch>
2018-03-08 23:39:11 -0500

    Extract `Database` module

---
+++ purescript/src/Signal/Database.purs
@@ -0,0 +1,60 @@
+module Signal.Database
+ ( getMessageById
+ , open
+ ) where
+
+import Prelude
+
+import Control.Monad.Aff                 (Aff)
+import Control.Monad.Except              (runExcept)
+import Data.Either                       (Either(..))
+import Data.Foreign                      (Foreign, ForeignError(..))
+import Data.List.Types                   (NonEmptyList)
+import Data.List.NonEmpty                as NonEmpty
+import Data.Maybe                        (Maybe(..), maybe)
+import Database.IndexedDB.Core           as IDB
+import Database.IndexedDB.IDBDatabase    as IDBDatabase
+import Database.IndexedDB.IDBFactory     as IDBFactory
+import Database.IndexedDB.IDBKeyRange    as IDBKeyRange
+import Database.IndexedDB.IDBObjectStore as IDBObjectStore
+import Database.IndexedDB.IDBTransaction as IDBTransaction
+
+import Signal.Types.Message (Message, readMessage)
+
+
+type MessageId = String
+
+-- meta
+databaseVersion :: Maybe Int
+databaseVersion = Just 16
+
+databaseName :: String
+databaseName = "signal"
+
+callbacks :: forall e. IDBFactory.Callbacks e
+callbacks = { onBlocked: Nothing
+            , onUpgradeNeeded: Nothing
+            }
+
+-- stores
+messageStoreName :: String
+messageStoreName = "messages"
+
+open
+  :: forall e
+  . Aff (idb :: IDB.IDB | e) IDB.Database
+open = IDBFactory.open databaseName databaseVersion callbacks
+
+getMessageById
+  :: forall e
+  .  IDB.Database
+  -> MessageId
+  -> Aff (idb :: IDB.IDB | e) (Either (NonEmptyList ForeignError) Message)
+getMessageById db mid = do
+    tx      <- IDBDatabase.transaction db [messageStoreName] IDB.ReadOnly
+    store   <- IDBTransaction.objectStore tx messageStoreName
+    message <- IDBObjectStore.get store (IDBKeyRange.only mid)
+    pure $ maybe (Left $ NonEmpty.singleton $ ForeignError "Not found") toEither message
+  where
+    toEither :: Foreign -> Either (NonEmptyList ForeignError) Message
+    toEither = runExcept <<< readMessage
