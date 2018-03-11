4996180c2a3ff9c7a68ecccbf6a5a112017f55c4
Daniel Gasienica <daniel@gasienica.ch>
2018-03-11 00:02:01 -0500

    Implement `DB.getAllConversations`

--- purescript/src/Signal/Database.purs
+++ purescript/src/Signal/Database.purs
@@ -1,6 +1,7 @@
 module Signal.Database
  ( getMessageById
  , getConversationById
+ , getAllConversations
  , open
  ) where
 
@@ -16,6 +17,7 @@ import Data.Maybe                        (Maybe(..), maybe)
 import Database.IndexedDB.Core           as IDB
 import Database.IndexedDB.IDBDatabase    as IDBDatabase
 import Database.IndexedDB.IDBFactory     as IDBFactory
+import Database.IndexedDB.IDBKey         as IDBKey
 import Database.IndexedDB.IDBKeyRange    as IDBKeyRange
 import Database.IndexedDB.IDBObjectStore as IDBObjectStore
 import Database.IndexedDB.IDBTransaction as IDBTransaction
@@ -78,6 +80,16 @@ getConversationById db cId = do
   where
     toEither :: Foreign -> Either (NonEmptyList ForeignError) Conversation
     toEither = runExcept <<< readConversation
+
+getAllConversations
+  :: forall e
+  .  IDB.Database
+  -> Aff (idb :: IDB.IDB | e) (Array IDBKey.Key)
+getAllConversations db = do
+    tx              <- IDBDatabase.transaction db [conversationsStoreName] IDB.ReadOnly
+    store           <- IDBTransaction.objectStore tx conversationsStoreName
+    conversationIds <- IDBObjectStore.getAllKeys store Nothing (Just 100)
+    pure $ conversationIds
 
 error :: forall a. String -> Either (NonEmptyList ForeignError) a
 error message = Left $ NonEmpty.singleton $ ForeignError message
