99011252c405be8a09804066d93f134a8bbe2614
Daniel Gasienica <daniel@gasienica.ch>
2018-03-18 09:37:26 -0400

    Add `Signal.Types.Timestamp` type

---
+++ purescript/src/Signal/Types/Timestamp.purs
@@ -0,0 +1,25 @@
+module Signal.Types.Timestamp
+  ( Timestamp(..)
+  , readTimestamp
+  ) where
+
+import Prelude
+
+import Data.Foreign (F, Foreign, ForeignError(..), fail, readNumber, typeOf)
+
+import Signal.Types.Long (Long, readLong)
+
+
+data Timestamp = TSLong Long | TSNumber Number
+
+derive instance eqTimestamp :: Eq Timestamp
+
+instance showTimestamp :: Show Timestamp where
+  show (TSLong ts)   = "(TSLong " <> show ts <> ")"
+  show (TSNumber ts) = "(TSNumber " <> show ts <> ")"
+
+readTimestamp :: Foreign -> F Timestamp
+readTimestamp value
+  | typeOf value == "object" = map TSLong $ readLong value
+  | typeOf value == "number" = map TSNumber $ readNumber value
+  | otherwise                = fail $ ForeignError "Invalid timestamp"
